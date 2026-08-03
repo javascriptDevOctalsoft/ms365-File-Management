f6d59d621e0a862c2d7965f9979a5cb524f5fc20
javascriptDevOctalsoft <110975665+javascriptDevOctalsoft@users.noreply.github.com>
2026-08-03 12:11:24 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -21,6 +21,7 @@ router.get("/login", async (req, res) => {
 
 router.get("/auth/callback", async (req, res) => {
         console.log("/auth/callback");
+		console.log("req.query.---->", req.query)
         const flag = req.query.state.flag;
         const selDocId = req.query.state.selDocId;
         const token = await cca.acquireTokenByCode({
