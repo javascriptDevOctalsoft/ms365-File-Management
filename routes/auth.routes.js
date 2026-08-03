e582d34cd4ae34fec6e1419f9749dd14993de75a
javascriptDevOctalsoft <110975665+javascriptDevOctalsoft@users.noreply.github.com>
2026-08-03 18:49:36 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,10 +5,14 @@ const { cca } = require("../config/msalConfig");
 router.get("/login", async (req, res) => {
         const flag = req.query.flag;
         const selDocId = req.query.curDocId;
+        const fileName = req.query.fileName+"_V"+req.query.verNo;
+        const savedFlag = req.query.saved;
         req.session.selectedFlag = flag;
         req.session.selDocId = selDocId;
+        req.session.fileName = fileName;
+        req.session.savedFlag = savedFlag;
         console.log("flag:", flag);
-		const state = JSON.stringify({ flag, selDocId });
+		const state = JSON.stringify({ flag, selDocId, fileName, savedFlag});
         const authUrl = await cca.getAuthCodeUrl({
                 scopes: ["User.Read", "offline_access"],
                 redirectUri: process.env.REDIRECT_URI,
@@ -22,6 +26,9 @@ router.get("/auth/callback", async (req, res) => {
         const state = req.query.state ? JSON.parse(req.query.state) : {};
 		const flag = state.flag;
 		const selDocId = state.selDocId;
+		const fileName = state.fileName;
+		const verNo = state.verNo;
+		const savedFlag = state.savedFlag;
 		console.log("selDocId---->", selDocId)
         const token = await cca.acquireTokenByCode({
                 code: req.query.code,
@@ -37,7 +44,7 @@ router.get("/auth/callback", async (req, res) => {
         if (flag) {
                 req.session.selectedFlag = flag;
         }
-        res.redirect("/?flag="+flag+"&selDocId="+selDocId);
+        res.redirect("/?flag="+flag+"&selDocId="+selDocId+"&fileName="+fileName+"&saved="+savedFlag);
 });
 
 
