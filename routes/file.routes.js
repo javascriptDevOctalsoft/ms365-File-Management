1aefc183e5d494dbf6ffea711fb1120dda8b7db0
javascriptDevOctalsoft <110975665+javascriptDevOctalsoft@users.noreply.github.com>
2026-08-03 19:02:39 +0530

    Update file.routes.js

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -13,6 +13,7 @@ router.post("/upload", ensureAuthenticated, upload.single("file"), async (req, r
                 const selDocId = req.query.flag || req.session.selDocId;
                 const fileName = req.query.fileName || req.session.fileName;
 				console.log("uploaded filename", fileName);
+				console.log("req original filename", req.file.originalname);
                 console.log("upload flag:", flag);
                 console.log("Selected Document Id:", selDocId);
                 const appToken = await getAppToken();
